e51445fe56dba4b598f53ece8372686743add991
Nadav Spiegelman <me@nadav.name>
2015-04-22 19:34:10 -0700

    Reindent with 2 spaces

--- app/App.js
+++ app/App.js
@@ -6,103 +6,100 @@ var sortBy = require('sort-by');
 var Paginator = require('react-pagify');
 
 var DataTable = React.createClass({
-    propTypes: {
-        data: React.PropTypes.array.isRequired,
-        columns: React.PropTypes.array.isRequired,
-        defaultSort: React.PropTypes.string.isRequired
-    },
+  propTypes: {
+    data: React.PropTypes.array.isRequired,
+    columns: React.PropTypes.array.isRequired,
+    defaultSort: React.PropTypes.string.isRequired
+  },
 
-    getInitialState () {
-        var filterQuery = this.props.columns.map(column => column.name)
-                .reduce((columns, i) => {
-                    columns[i] = '';
-                    return columns;
-                }, {});
+  getInitialState () {
+    var filterQuery = this.props.columns.map(column => column.name)
+    .reduce((columns, i) => {
+      columns[i] = '';
+      return columns;
+    }, {});
 
-        return {
-            data: this.props.data,
-            dataColumns: this.props.columns,
-            sortBy: {
-                column: this.props.defaultSort,
-                direction: ''
-            },
-            filteredData: this.props.data.sort(sortBy(this.props.defaultSort)),
-            filterQuery,
-            pagination: {
-                page: 0,
-                perPage: 10
-            }
-        };
-    },
-
-    handleFilter (filterQuery) {
-        this.setState({ filterQuery }, this.filterData);
-    },
-
-    filterData () {
-        var filteredIndexes = this.state.data.map((item) => {
-            return this.state.dataColumns.map(column =>
-                item[column.name].toLowerCase().
-                    indexOf(this.state.filterQuery[column.name].toLowerCase()));
-            }).map((item) => {
-                return item.reduce((previous, current) => {
-                    return Math.min(previous, current);
-                });
-            });
-
-        var filteredData = this.state.data.filter((item, i) => {
-            return filteredIndexes[i] > -1;
-        }).sort(sortBy(this.state.sortBy.direction + this.state.sortBy.column));
-
-        this.setState({ filteredData });
-    },
-
-        handleSort (column) {
-        var direction = '';
-
-        // If already sorting by the column, switch direction
-        if (this.state.sortBy.column === column.name && this.state.sortBy.direction === '') {
-            direction = '-';
+    return {
+        data: this.props.data,
+        dataColumns: this.props.columns,
+        sortBy: {
+            column: this.props.defaultSort,
+            direction: ''
+        },
+        filteredData: this.props.data.sort(sortBy(this.props.defaultSort)),
+        filterQuery,
+        pagination: {
+            page: 0,
+            perPage: 10
         }
+    };
+  },
 
-        this.setState({ sortBy: { column: column.name, direction } }, this.filterData);
-    },
+  handleFilter (filterQuery) {
+    this.setState({ filterQuery }, this.filterData);
+  },
 
-    handlePagination (page) {
-      const { pagination } = this.state;
-      pagination.page = page;
-      this.setState({ pagination });
-    },
+  filterData () {
+    var filteredIndexes = this.state.data.map((item) => {
+      return this.state.dataColumns.map(column =>
+        item[column.name].toLowerCase().
+        indexOf(this.state.filterQuery[column.name].toLowerCase()));
+      }).map((item) => {
+        return item.reduce((previous, current) => {
+          return Math.min(previous, current);
+        });
+    });
 
-    render () {
-        const paginated = Paginator.paginate(this.state.filteredData, this.state.pagination);
+    var filteredData = this.state.data.filter((item, i) => {
+      return filteredIndexes[i] > -1;
+    }).sort(sortBy(this.state.sortBy.direction + this.state.sortBy.column));
 
-        return (
-            <table className="pure-table">
-                <TableHead
-                    data={this.props.data}
-                    columns={this.state.dataColumns}
-                    filterQuery={this.state.filterQuery}
-                    handleFilter={this.handleFilter}
-                    sortBy={this.state.sortBy}
-                    handleSort={this.handleSort}
-                />
-                <TableBody
-                    filteredData={paginated.data}
-                    columns={this.state.dataColumns}
-                />
-                <tfoot>
-                    <Paginator
-                        page={paginated.page}
-                        pages={paginated.amount}
-                        beginPages={3}
-                        endPages={3}
-                        onSelect={this.handlePagination}
-                        />
-                </tfoot>
-            </table>
-        );
+    this.setState({ filteredData });
+  },
+
+  handleSort (column) {
+    var direction = '';
+
+    // If already sorting by the column, switch direction
+    if (this.state.sortBy.column === column.name && this.state.sortBy.direction === '') {
+      direction = '-';
     }
+
+    this.setState({ sortBy: { column: column.name, direction } }, this.filterData);
+  },
+
+  handlePagination (page) {
+    const { pagination } = this.state;
+    pagination.page = page;
+    this.setState({ pagination });
+  },
+
+  render () {
+    const paginated = Paginator.paginate(this.state.filteredData, this.state.pagination);
+
+    return (
+      <table className="pure-table">
+        <TableHead
+          data={this.props.data}
+          columns={this.state.dataColumns}
+          filterQuery={this.state.filterQuery}
+          handleFilter={this.handleFilter}
+          sortBy={this.state.sortBy}
+          handleSort={this.handleSort} />
+        <TableBody
+          filteredData={paginated.data}
+          columns={this.state.dataColumns} />
+        <tfoot>
+          <Paginator
+            page={paginated.page}
+            pages={paginated.amount}
+            beginPages={3}
+            endPages={3}
+            onSelect={this.handlePagination} />
+        </tfoot>
+      </table>
+    );
+  }
 });
 
 React.render(
